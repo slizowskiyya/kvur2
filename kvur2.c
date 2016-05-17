124fee5f6fc946b9d0363db41a0fbe3f0fc47e2b
Слизов Константин Васильевич <ri58s17@awp10.csc.local>
2016-05-17 08:26:58 +0600

    krasivo?

--- kvur2.c
+++ kvur2.c
@@ -5,39 +5,39 @@ void kvur(float a, float b, float c);
 
 int main()
 {
-    float a, b, c;
+	float a, b, c;
 
-    printf("Input a: ");
-    scanf("%d", &a);
-    printf("Input b: ");
-    scanf("%d", &b);
-    printf("Input c: ");
-    scanf("%d", &c);
+	printf("Input a: ");
+	scanf("%d", &a);
+	printf("Input b: ");
+	scanf("%d", &b);
+	printf("Input c: ");
+	scanf("%d", &c);
 
-    kvur(a, b, c);
+	kvur(a, b, c);
 
-    return 0;
+	return 0;
 }
 
 void kvur(float a, float b, float c)
 {
-    float d, x1, x2;
+	float d, x1, x2;
 
-    d=(b*b)-(4*a*c);
+	d = (b * b) - (4 * a * c);
 
-    if (d==0)
-    {
-        x1=(-b)/(2*a);
-        printf("x = %.2f", x1);
-    }
+	if (d == 0)
+	{
+		x1 = (-b) / (2 * a);
+		printf("x = %.2f", x1);
+	}
 
-    if(d>0)
-    {
-        x1=((-b)+sqrt(d))/(2*a);
-        x2=((-b)-sqrt(d))/(2*a);
-        printf("x1 = %.2f\nx2 = %.2f", x1, x2);
-    }
+	if (d > 0)
+	{
+		x1 = ((-b) + sqrt(d)) / (2 * a);
+		x2 = ((-b) - sqrt(d)) / (2 * a);
+		printf("x1 = %.2f\nx2 = %.2f", x1, x2);
+	}
 
-    if(d<0)
-        printf("No answer");
+	if (d < 0)
+		printf("No answer");
 }
